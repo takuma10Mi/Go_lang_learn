51a10431976fa8712ff15af91073ae2ffc9d3f40
takuma10Mi <amaebilove@gmail.com>
2020-09-23 21:43:44 +0900

    commit

--- JSONAPP/JSONAPP/ContentView.swift
+++ JSONAPP/JSONAPP/ContentView.swift
@@ -5,17 +5,42 @@
 //  Created by 南端拓磨 on 2020/09/22.
 //
 
-import SwiftUI
+import UIKit
+import Alamofire
+import SwiftyJSON
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // APIリクエストの関数を呼び出す
+        getArticles()
+    }
+
+    func getArticles() {
+        // Alamofireを使う
+        // URLでリクエストを飛ばし、レスポンスがresponseに入る
+        AF.request("https://fiber.fibe-group.com//wp-json/wp/v2/posts?_embed").responseJSON { response in
+        // 確認のためresponseを見てみる
+            print("Response JSON: \(String(describing: response.value))")
+
+        // responseがnilじゃなかったら変数jsonObjectにレスポンスを入れる
+        if let jsonObject = response.value {
+
+                // 変数jsonにJSON形式にしたjsonObjectを入れる
+                let json = JSON(jsonObject)
+
+                // 配列の数だけ処理を繰り返す
+                for i in 0..<json.count {
+                    let array = json[i]
+                    // "taitle"の"rendered"を指定してあげると記事タイトルが取れる
+                    let title = array["title"]["rendered"].stringValue
+
+                    // 取得できたタイトルを表示
+                    print(title)
+                }
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
---
+++ JSONAPP/Pods/Target Support Files/Pods-JSONAPPTests/Pods-JSONAPPTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JSONAPPTests : NSObject
+@end
+@implementation PodsDummy_Pods_JSONAPPTests
+@end
