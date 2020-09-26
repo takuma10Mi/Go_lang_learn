39d9585d2d08432ddf04c8aedafb0acb0c876b9f
takuma10Mi <amaebilove@gmail.com>
2020-09-26 15:26:53 +0900

    commit

--- json ui/json ui/ContentView.swift
+++ json ui/json ui/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var posts: [Post] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(posts){ post in
+            Text(post.title)
+        }
+        .onAppear{
+            Api().getPosts { (posts) in
+                self.posts = posts
+            }
+            
+        }
     }
 }
 
---
+++ json ui/json ui/PostList.swift
@@ -0,0 +1,20 @@
+//
+//  PostList.swift
+//  json ui
+//
+//  Created by 南端拓磨 on 2020/09/26.
+//
+
+import SwiftUI
+
+struct PostList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostList_Previews: PreviewProvider {
+    static var previews: some View {
+        PostList()
+    }
+}
