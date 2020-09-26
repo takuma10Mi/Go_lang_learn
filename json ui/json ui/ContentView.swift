342e63a8dcbb4d29a34d3fb2382d08a4f7f20902
takuma10Mi <amaebilove@gmail.com>
2020-09-26 14:36:42 +0900

    commit

---
+++ json ui/json ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  json ui
+//
+//  Created by 南端拓磨 on 2020/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ json ui/json ui/json_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_uiApp.swift
+//  json ui
+//
+//  Created by 南端拓磨 on 2020/09/26.
+//
+
+import SwiftUI
+
+@main
+struct json_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
